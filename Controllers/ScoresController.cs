9fb07a0d5941c9320943c7b5181f9f06d07ace9d
Joy Kimaru <Joy Kimaru>
2017-09-25 15:53:41 -0700

    initial commit

---
+++ Controllers/ScoresController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/Scores")]
+    public class ScoresController : Controller
+    {
+        // GET api/scores
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "score1", "score2" };
+        }
+
+        // GET api/scores/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "score";
+        }
+
+        // POST api/scores
+        [HttpPost]
+        public void Post([FromBody]string score)
+        {
+        }
+
+        // PUT api/scores/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string score)
+        {
+        }
+
+        // DELETE api/scores/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
