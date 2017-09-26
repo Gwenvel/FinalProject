224bb8a76d8f62a9afa71a962117bd2736e1af2d
Jason Tennant <xcodula@icloud.com>
2017-09-25 18:18:30 -0700

    updates

--- wwwroot/ngApp/controllers/testController.js
+++ wwwroot/ngApp/controllers/testController.js
@@ -3,5 +3,9 @@ class TestController {
         this.message = "this is the test view";
         // var gameInstance = UnityLoader.instantiate("gameContainer", "Build/Builds.json", {onProgress: UnityProgress});
         var gameInstance = UnityLoader.instantiate("gameContainer", "Build/game.json", {onProgress: UnityProgress});
+
+        // https://codetolight.wordpress.com/2017/01/18/unity-rest-api-interaction-with-unitywebrequest-and-besthttp/
+        // https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Post.html
+        // https://docs.unity3d.com/Manual/UnityWebRequest-RetrievingTextBinaryData.html
     }
 }
